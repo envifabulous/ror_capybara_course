88ea8539aff6f62a2c8c988f77532f2900d2d28c
envifabulous <envijesus666@gmail.com>
2022-02-17 07:53:09 +0000

    Create Article Successfully

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,26 @@
+class ArticlesController < ApplicationController
+  def index
+  end
+	
+	def new
+		@article = Article.new
+	end
+	
+	def create
+		@article = Article.new(article_params)
+		@article.save
+		flash[:success] = "Article has been created"
+		redirect_to articles_path
+	end
+	
+	private
+	
+		def article_params
+			params.require(:article).permit(:title, :body)
+		end
+	
+	def verify_authenticity_token
+		request.headers['X-CSRF-Token'] ||= request.headers['X-XSRF-TOKEN']
+	end
+	
+end
